00678244fbcf6ce20c237d3f0e304eeefaa04d02
Jatin Gupta <jatingupta4385@gmail.com>
2025-06-20 17:47:25 +0530

    Fix layout issue on HomePage

--- chatty-app/src/pages/HomePage.jsx
+++ chatty-app/src/pages/HomePage.jsx
@@ -12,12 +12,12 @@ function HomePage() {
       <div className="backdrop-blur-xl p-8 rounded-2xl shadow-lg border border-white/10 w-[90%] h-[80%] flex gap-4">
         
         {/* Sidebar - 25% */}
-        <div className="basis-1/4 h-full overflow-hidden">
+        <div className="basis-1/4 h-full ">
           <Sidebar />
         </div>
 
         {/* ChatContainer - 50% */}
-        <div className="basis-2/4 h-full overflow-hidden">
+        <div className="basis-2/4 h-full n">
           <ChatContainer />
         </div>
 
